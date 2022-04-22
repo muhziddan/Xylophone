b7d7d06261f5a6a354af7131a456002cb8f2a6f0
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-04-22 23:39:15 +0700

    Link all button to one IBAction, make the playSound method more concise

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -11,43 +11,33 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        // test
-    }
-
-    @IBAction func cKeyPressed(_ sender: UIButton) {
-        playSound(key: "C")
-        print("pressed C")
     }
     
-    @IBAction func dKeyPressed(_ sender: UIButton) {
-        playSound(key: "D")
-        print("Pressed D")
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        guard let title = sender.titleLabel?.text else { return }
+        
+        playSound(key: title)
     }
-    
     
     func playSound(key: String) {
-        guard let urlC = Bundle.main.url(forResource: "Sounds/C", withExtension: "wav") else { return }
         
-        guard let urlD = Bundle.main.url(forResource: "Sounds/D", withExtension: "wav") else { return }
-
+        guard let url = Bundle.main.url(forResource: "Sounds/\(key)", withExtension: "wav") else { return }
+        
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
             try AVAudioSession.sharedInstance().setActive(true)
-
-            if (key == "C") {
-                player = try AVAudioPlayer(contentsOf: urlC, fileTypeHint: AVFileType.mp3.rawValue)
-            } else if (key == "D") {
-                player = try AVAudioPlayer(contentsOf: urlD, fileTypeHint: AVFileType.mp3.rawValue)
-            }
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
             
             guard let player = player else { return }
-
+            
             player.play()
-
+            
         } catch let error {
             print(error.localizedDescription)
         }
